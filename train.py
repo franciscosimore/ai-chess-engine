6779b61eea86991ddce5dbfa386a6c896466a624
Francisco Simore <fsimore@corebi.com.ar>
2023-10-24 18:29:22 -0300

    implement first working train

--- train.py
+++ train.py
@@ -23,21 +23,21 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
 
-        self.a1 = nn.Conv2d(5, 16, kernel_size=3)
-        self.a2 = nn.Conv2d(16, 16, kernel_size=3)
-        self.a3 = nn.Conv2d(16, 32, kernel_size=3)
+        self.a1 = nn.Conv2d(5, 16, kernel_size=3, padding=1)
+        self.a2 = nn.Conv2d(16, 16, kernel_size=3, padding=1)
+        self.a3 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
 
-        self.b1 = nn.Conv2d(32, 32, kernel_size=3)
-        self.b2 = nn.Conv2d(32, 32, kernel_size=3)
-        self.b3 = nn.Conv2d(32, 64, kernel_size=3)
+        self.b1 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
+        self.b2 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
+        self.b3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
 
-        self.c1 = nn.Conv2d(64, 64, kernel_size=3)
-        self.c2 = nn.Conv2d(64, 64, kernel_size=3)
-        self.c3 = nn.Conv2d(64, 128, kernel_size=3)
+        self.c1 = nn.Conv2d(64, 64, kernel_size=2, padding=1)
+        self.c2 = nn.Conv2d(64, 64, kernel_size=2, padding=1)
+        self.c3 = nn.Conv2d(64, 128, kernel_size=2, padding=1)
 
-        self.d1 = nn.Conv2d(128, 128, kernel_size=3)
-        self.d2 = nn.Conv2d(128, 128, kernel_size=3)
-        self.d3 = nn.Conv2d(128, 128, kernel_size=3)
+        self.d1 = nn.Conv2d(128, 128, kernel_size=1, padding=1)
+        self.d2 = nn.Conv2d(128, 128, kernel_size=1, padding=1)
+        self.d3 = nn.Conv2d(128, 128, kernel_size=1, padding=1)
 
         self.last = nn.Linear(128, 1)
 
@@ -45,19 +45,19 @@ class Net(nn.Module):
         x = F.relu(self.a1(x))
         x = F.relu(self.a2(x))
         x = F.relu(self.a3(x))
-        x = F.max_pool2d(x)
+        x = F.max_pool2d(x, 2)
 
         # 4x4
         x = F.relu(self.b1(x))
         x = F.relu(self.b2(x))
         x = F.relu(self.b3(x))
-        x = F.max_pool2d(x)
+        x = F.max_pool2d(x, 2)
 
         # 2x2
         x = F.relu(self.c1(x))
         x = F.relu(self.c2(x))
         x = F.relu(self.c3(x))
-        x = F.max_pool2d(x)
+        x = F.max_pool2d(x, 2)
 
         # 1x128
         x = F.relu(self.d1(x))
@@ -68,21 +68,26 @@ class Net(nn.Module):
         x = self.last(x)
 
         # Value output
-        return F.log_softmax(x)
+        return F.sigmoid(x)
 
 chess_dataset = ChessValueDataset()
 train_loader = torch.utils.data.DataLoader(chess_dataset)
 model = Net()
 optimizer = optim.Adam(model.parameters())
+loss = nn.MSELoss()
 
-device = "cuda"
+device = "cpu"
 
 model.train()
 for batch_idx, (data, target) in enumerate(train_loader):
-    print(data)
-    print(target)
+    target = target.unsqueeze(-1)
     data, target = data.to(device), target.to(device)
+    data = data.float()
+    target = target.float()
+
     optimizer.zero_grad()
     output = model(data)
-    loss = F.nll_loss(output, target)
-    loss.backward()
+
+    loss_t = loss(output, target)
+    loss_t.backward()
+    optimizer.step()
