02e1926ab2d515fe7273730aa3acabfeaab5aa11
Francisco Simore <franciscosimore@gmail.com>
2023-10-25 17:28:27 -0300

    implement simple html site with flash

--- play.py
+++ play.py
@@ -1,7 +1,10 @@
 import chess
+import chess.svg
+from flask import Flask, Response
 import torch
 from state import State
 from train import Net
+import time
 
 class Valuator(object):
     def __init__(self):
@@ -22,12 +25,51 @@ def explore_leaves(s, v):
         s.board.pop()
     return ret
 
+v = Valuator()
+s = State()
+
+def computer_move():
+    move = sorted(explore_leaves(s, v), key=lambda x: x[0], reverse=s.board.turn)[0]
+    print(move)
+    s.board.push(move[1])
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return """
+<html>
+  <head>
+    <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js'></script>
+    <style>
+      button {
+        font-size: 20px;
+      }
+    </style>
+  </head>
+  <body>
+    <img width="600" height="600" src="board.svg?%f"/><br>
+    <button onclick="console.log('Button Clicked'); $.post('/move', function(data) { console.log('Response:', data); location.reload(); });">Make Computer Move</button>
+  </body>
+</html>
+""" % time.time()
+
+@app.route("/board.svg")
+def board():
+    return Response(chess.svg.board(board=s.board), mimetype="image/svg+xml")
+
+@app.route("/move", methods=["POST"])
+def move():
+    computer_move()
+    return("")
+
 if __name__ == "__main__":
-    v = Valuator()
-    s = State()
-    while not s.board.is_game_over():
-        l = sorted(explore_leaves(s, v), key=lambda x: x[0], reverse=s.board.turn)
-        move = l[0]
-        print(move)
-        s.board.push(move[1])
-    print(s.board.result())
+    app.run(debug=True)
+
+# Self Play
+# while not s.board.is_game_over():
+#     l = sorted(explore_leaves(s, v), key=lambda x: x[0], reverse=s.board.turn)
+#     move = l[0]
+#     print(move)
+#     s.board.push(move[1])
+# print(s.board.result())
